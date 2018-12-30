43110e811555169fcb6b9c4b008571236d95ab7a
Tom Verkon <tverkon@usa.net>
2018-12-30 16:53:13 -0500

    Initial Project Setup with Initializr

---
+++ src/test/java/com/diligentgroup/sfgthvpetclinic/SfgThvPetClinicApplicationTests.java
@@ -0,0 +1,17 @@
+package com.diligentgroup.sfgthvpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgThvPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
