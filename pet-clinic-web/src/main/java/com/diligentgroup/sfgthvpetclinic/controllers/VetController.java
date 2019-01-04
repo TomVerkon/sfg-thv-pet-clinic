328a08e0851bf599c48baca8981598ea3604d1f0
Tom Verkon <tverkon@usa.net>
2019-01-04 11:35:15 -0500

    IMplemented Owner and Vet Controller with RequestMappings at the calss level. Added Owners index page. Close #13

--- pet-clinic-web/src/main/java/com/diligentgroup/sfgthvpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/diligentgroup/sfgthvpetclinic/controllers/VetController.java
@@ -4,8 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.diligentgroup.sfgthvpetclinic.services.VetService;
-
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 	
@@ -15,7 +14,7 @@ public class VetController {
 //		this.vetService = vetService;
 //	}
 	
-	@RequestMapping({"/vets", "vets/index", "vets/index.html"})
+	@RequestMapping({"", "index", "index.html"})
 	public String vetList(Model model) {
 		//model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
