487c7b9fbf4abd24ed78c0c5cc8ef5e2c5e9010d
Tom Verkon <tverkon@usa.net>
2019-01-11 16:18:22 -0500

    Created Pet Type map service impl Closes #30

---
+++ pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.diligentgroup.sfgthvpetclinic.services;
+
+import com.diligentgroup.sfgthvpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
