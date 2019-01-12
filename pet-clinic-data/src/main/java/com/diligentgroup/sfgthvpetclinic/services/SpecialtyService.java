509579692c3b4a3c790cac6aed40c81428175ad4
Tom Verkon <tverkon@usa.net>
2019-01-11 23:44:30 -0500

    Created Specialty map Service closes #32

---
+++ pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/services/SpecialtyService.java
@@ -0,0 +1,7 @@
+package com.diligentgroup.sfgthvpetclinic.services;
+
+import com.diligentgroup.sfgthvpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long>{
+
+}
