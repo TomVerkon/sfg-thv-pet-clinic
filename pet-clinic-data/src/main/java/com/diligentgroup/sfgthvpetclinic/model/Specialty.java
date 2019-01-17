b29c26be57e6c5414a436ee9ee50095be2a01035
Tom Verkon <tverkon@usa.net>
2019-01-17 11:47:38 -0500

    Converted Vet and Specialty to JPA Entities CLoses #38

--- pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/model/Specialty.java
@@ -1,13 +1,18 @@
 package com.diligentgroup.sfgthvpetclinic.model;
 
-import lombok.ToString;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 8574153354221522230L;
+	@Column(name = "description")
 	private String description;
 
 	public String getDescription() {
--- pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/model/Vet.java
@@ -3,14 +3,27 @@ package com.diligentgroup.sfgthvpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
-import lombok.ToString;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2346089693183540530L;
+	
+	@ManyToMany(cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_specialties",
+			joinColumns = @JoinColumn(name = "vet_id"), 
+			inverseJoinColumns = @JoinColumn(name = "specialty_id"))
 	private Set<Specialty> specialties;
 
 	public Set<Specialty> getSpecialties() {
