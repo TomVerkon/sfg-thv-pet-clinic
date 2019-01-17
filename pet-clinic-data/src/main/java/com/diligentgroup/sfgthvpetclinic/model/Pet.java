1a80262c8f1e73f682a7103dd4bb5e5b97d3118c
Tom Verkon <tverkon@usa.net>
2019-01-17 11:59:01 -0500

    Created Visit Entity closes #39

--- pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package com.diligentgroup.sfgthvpetclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -36,6 +40,9 @@ public class Pet extends BaseEntity {
 	
 	@Column(name = "name")
 	private String name;
+	
+	@OneToMany(mappedBy = "pet", cascade = CascadeType.ALL)
+	private Set<Visit> visits;
 
 	public PetType getPetType() {
 		return petType;
@@ -67,6 +74,17 @@ public class Pet extends BaseEntity {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Set<Visit> getVisits() {
+		if (this.visits == null) {
+			this.visits = new HashSet<Visit>();
+		}
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 
 }
--- pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/model/Visit.java
@@ -2,17 +2,30 @@ package com.diligentgroup.sfgthvpetclinic.model;
 
 import java.time.LocalDate;
 
-import lombok.ToString;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -783205217477881233L;
+	
+	@Column(name = "date")
 	private LocalDate date;
+	
+	@Column(name = "description")
 	private String description;
-	private Pet pets;
+	
+	@ManyToOne
+	@JoinColumn(name = "pet_id", referencedColumnName = "id", nullable = false)
+	private Pet pet;
 
 	public LocalDate getDate() {
 		return date;
@@ -30,11 +43,11 @@ public class Visit extends BaseEntity {
 		this.description = description;
 	}
 
-	public Pet getPets() {
-		return pets;
+	public Pet getPet() {
+		return pet;
 	}
 
-	public void setPets(Pet pets) {
-		this.pets = pets;
+	public void setPets(Pet pet) {
+		this.pet = pet;
 	}
 }
