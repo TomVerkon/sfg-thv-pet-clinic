dbd6f27e99833fee4455c70758ef69be21ad2581
Tom Verkon <tverkon@usa.net>
2019-01-17 15:27:49 -0500

    Created Spring Data JPA Service for Specialty closes #45

---
+++ pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/services/springdatajpa/SpecialtyServiceJpa.java
@@ -0,0 +1,49 @@
+package com.diligentgroup.sfgthvpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+
+import com.diligentgroup.sfgthvpetclinic.model.Specialty;
+import com.diligentgroup.sfgthvpetclinic.repositories.SpecialtyRepository;
+import com.diligentgroup.sfgthvpetclinic.services.SpecialtyService;
+
+@Profile("springdatajpa")
+public class SpecialtyServiceJpa implements SpecialtyService {
+	
+	private SpecialtyRepository specialtyRepository;
+
+	public SpecialtyServiceJpa(SpecialtyRepository specialtyRepository) {
+		super();
+		this.specialtyRepository = specialtyRepository;
+	}
+
+	@Override
+	public Set<Specialty> findAll() {
+		Set<Specialty> specialties = new HashSet<Specialty>();
+		specialtyRepository.findAll().forEach(specialties::add);
+		return specialties;
+	}
+
+	@Override
+	public Specialty findById(Long id) {
+		return specialtyRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Specialty save(Specialty object) {
+		return specialtyRepository.save(object);
+	}
+
+	@Override
+	public void delete(Specialty object) {
+		specialtyRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		specialtyRepository.deleteById(id);
+	}
+
+}
