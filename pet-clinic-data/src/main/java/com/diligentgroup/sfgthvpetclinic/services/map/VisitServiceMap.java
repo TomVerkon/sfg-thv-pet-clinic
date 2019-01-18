df39894a4334c509ce0eb6bccf164d9a48d1218d
Tom Verkon <tverkon@usa.net>
2019-01-18 11:32:28 -0500

    Created a Visit Map Service closes #48. Also added isNew to BaseEntity as helper method

---
+++ pet-clinic-data/src/main/java/com/diligentgroup/sfgthvpetclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,46 @@
+package com.diligentgroup.sfgthvpetclinic.services.map;
+
+import java.util.Set;
+
+import com.diligentgroup.sfgthvpetclinic.model.Visit;
+import com.diligentgroup.sfgthvpetclinic.services.VisitService;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService{
+	
+	
+
+	@Override
+	public Set<Visit> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Visit findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Visit save(Visit visit) {
+		if (visit.getPet() == null 
+				|| visit.getPet().isNew() 
+				|| visit.getPet().getOwner() == null 
+				|| visit.getPet().getOwner().isNew()) {
+			throw new RuntimeException("Invalid Visit, missing or unpersisted Pet or Pet Owner!");
+		} else 
+		return super.save(visit);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Visit object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+}
